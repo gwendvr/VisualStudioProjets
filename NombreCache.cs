108f3893dbf0c818396c6e7a83057b62620c734a
Gwenaelle <75257467+gwendvr@users.noreply.github.com>
2021-03-25 16:15:18 +0100

    Nombre Cache
    Jeu où l'on doit chercher un nombre et le retrouver.

---
+++ NombreCache.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Programme du nombre caché
+              * Un joueur donne un nombre qu'il faut retrouver
+              * Date : 25/03/2021
+              * Auteur : Gwen */
+
+            // Déclarations
+            int nb = 0, essai, compteur = 1;
+            bool correct = false;
+
+            // Nombre à chercher
+            while (!correct)
+            {
+                try
+                {
+                    Console.WriteLine("Entrez le nombre à chercher =");
+                    nb = int.Parse(Console.ReadLine());
+                    correct = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Veuillez saisir un nombre.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+
+            // Recherche du nombre
+            Console.Clear();
+            Console.WriteLine("Entrez un essai =");
+            essai = int.Parse(Console.ReadLine());
+
+            //Boucle pour trouver le nombre
+            while (essai != nb)
+            {
+                if (essai > nb)
+                {
+                    Console.WriteLine("C'est trop grand !");
+                }
+                else
+                {
+                    Console.WriteLine("C'est trop petit !");
+                }
+                Console.WriteLine("Entrez un essai =");
+                essai = int.Parse(Console.ReadLine());
+                compteur++;
+            }
+            // Nombre trouvé
+            Console.Clear();
+            Console.WriteLine("Bravo vous avez trouver le bon nombre en " + compteur + " fois !");
+
+            Console.ReadLine();
+        }
+    }
+}
